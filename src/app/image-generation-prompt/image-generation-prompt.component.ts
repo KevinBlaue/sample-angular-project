c248ce2d34fa061d05f62c34b58223052417a082
Kevin Blaue <kevin@blaue-online.com>
2025-03-10 10:56:14 +0100

    Add dev environment

--- src/app/image-generation-prompt/image-generation-prompt.component.ts
+++ src/app/image-generation-prompt/image-generation-prompt.component.ts
@@ -5,7 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
